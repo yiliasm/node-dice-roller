3778dceb31c5ff1cfecdd5f22d5fcf0addfd8b1d
yiliasm <yiliasm@gmail.com>
2025-10-03 12:34:33 -0500

    update cors

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+//require cors
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+//use cors
+app.use(cors());
 
 //wake up 
 app.get('/wake', (req, res) => {
